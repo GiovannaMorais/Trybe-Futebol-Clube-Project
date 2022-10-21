3db366a4fa1d277155374b00524ed4862d9f78a7
GiovannaMorais <gigimorais9@hotmail.com>
2022-10-21 16:59:32 -0300

    Feat: create matches model

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,48 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+class Matches extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default Matches;
