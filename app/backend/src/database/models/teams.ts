950e604c0d240c25bd56336a1f8425dd46588c06
GiovannaMorais <gigimorais9@hotmail.com>
2022-10-21 16:58:57 -0300

    Feat: create teams model

---
+++ app/backend/src/database/models/teams.ts
@@ -0,0 +1,31 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+import Matches from './matches';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Teams;
