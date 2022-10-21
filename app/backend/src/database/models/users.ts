17e5ee791c5418646c4ad2ae33c9afe5c4c1ee5a
GiovannaMorais <gigimorais9@hotmail.com>
2022-10-21 16:57:54 -0300

    Feat: create users model

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,43 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
