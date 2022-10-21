0d2ffb1d0b09d113a0c192c1274119c5918a961a
GiovannaMorais <gigimorais9@hotmail.com>
2022-10-21 16:58:33 -0300

    Feat: create teams migration

---
+++ app/backend/src/database/migrations/202210211552-create-teams.js
@@ -0,0 +1,21 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      await queryInterface.createTable('teams', {
+        id: {
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        teamName: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          field: 'team_name',
+        }
+      });
+    },
+  
+    down: async (queryInterface, Sequelize) => {
+      await queryInterface.dropTable('teams');
+    }
+  };
